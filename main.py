de52c1c68f959c3d4001f1c42df2b6678109b014
Oleg <Zelo78@yandex.ru>
2022-07-21 11:36:01 +0300

    Remove DB

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-from fastapi import Depends, FastAPI, HTTPException, WebSocket, Cookie
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-from sqlalchemy.orm import Session
-
-import models
-import schemas
-from database import SessionLocal, engine
-
-models.Base.metadata.create_all(bind=engine)
-
 
 app = FastAPI()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.get("/")
-async def root(db: Session = Depends(get_db)):
-    db_sleep = models.Sleep()
-    db.add(db_sleep)
-    db.commit()
-    db.refresh(db_sleep)
-    sleep_id = db_sleep.id
-
+async def root():
     script = r"""
             const chatSocket = new WebSocket('ws://' + window.location.host + '/ws/add_thing/');
             
@@ -73,16 +50,14 @@ async def root(db: Session = Depends(get_db)):
                 <title>Sleep log</title>
             </head>
             <body>
-                <h1>Сон номер {sleep_id}</h1>
+                <h1>Ваш сон</h1>
                 <h2>Всё, что Вы увидели в этом сне</h2>
                 <text-area readonly id="sleep-log">
                 </text-area>
                 <hr>
                 <p>Вводите то, что Вы видите во сне, по одной мысли за раз, и нажимайте кнопку "Я увидел!"</p>  
-                <form action='#'>
-                    <input id="sleep-log-input" type="text" size="100"><br>
-                    <input id="sleep-log-submit" type="button" value="Я увидел!">
-                </form>
+                <input id="sleep-log-input" type="text" size="100"><br>
+                <input id="sleep-log-submit" type="button" value="Я увидел!">
                 
                 <script>
                 {script}
@@ -91,15 +66,12 @@ async def root(db: Session = Depends(get_db)):
         </html>
         """
     response = HTMLResponse(content=content)
-    response.set_cookie(key="sleep_id", value=str(sleep_id))
     return response
 
 
 @app.websocket("/ws/add_thing/")
 async def websocket_endpoint(
     websocket: WebSocket,
-    sleep_id: str | None = Cookie(default=None),
-    db: Session = Depends(get_db),
 ):
     number = 0
     await websocket.accept()
@@ -109,24 +81,8 @@ async def websocket_endpoint(
             data = await websocket.receive_json()
             thing = data.get("thing")
             number += 1
-
-            db_thing = models.Thing(number=number, name=thing, owner_id=sleep_id)
-            db.add(db_thing)
-            db.commit()
-            db.refresh(db_thing)
-
             await websocket.send_json({"thing": thing, "number": number})
         except Exception as e:
             print("error:", e)
             break
     print("Bye..")
-
-
-@app.get("/sleeps/", response_model=list[schemas.Sleep])
-async def get_sleeps(db: Session = Depends(get_db)):
-    return db.query(models.Sleep).all()
-
-
-@app.get("/things/", response_model=list[schemas.Thing])
-async def get_sleeps(db: Session = Depends(get_db)):
-    return db.query(models.Thing).all()
